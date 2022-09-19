1f8a579f9c13f78a99e210865769c65d4c006f21
Steven Anderson <steven.a.anderson2012@gmail.com>
2022-09-19 16:20:58 -0600

    Add command line arguments handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let _api_token = std::env::var("API_TOKEN").expect("Expected and API token.");
+    let mut arg_iterator = std::env::args();
+    arg_iterator.next();
+    let args: String = arg_iterator.collect();
+
+    dbg!(args);
 }
