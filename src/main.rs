fbb578e1edc8a9edbdaaef1abedb19f930124e58
Steven Anderson <steven.a.anderson2012@gmail.com>
2022-09-19 16:34:56 -0600

    Finish weather cli app tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 fn main() {
-    let _api_token = std::env::var("API_TOKEN").expect("Expected and API token.");
+    let api_token = std::env::var("API_TOKEN").expect("Expected and API token.");
     let mut arg_iterator = std::env::args();
     arg_iterator.next();
     let args: String = arg_iterator.collect();
 
-    dbg!(args);
+    let client = reqwest::blocking::Client::new();
+
+    let response = client
+        .get("https://api.waqi.info/search/")
+        .query(&[("token", api_token), ("keyword", args)])
+        .send()
+        .expect("A successful request")
+        .json::<serde_json::Value>()
+        .expect("Expected body to be json");
+    
+        dbg!(&response["data"]);
 }
